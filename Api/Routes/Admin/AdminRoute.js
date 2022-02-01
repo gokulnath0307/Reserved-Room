f13f35e68c1afeb3f956698164522d742bf3d7be
GOKULNATH S <74148032+gokulnath0307@users.noreply.github.com>
2022-02-01 20:52:42 +0530

    Delete Api directory

--- Api/Routes/Admin/AdminRoute.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const Admin=require('../../Controller/Admin/AdminController')
-
-router.post('/register',Admin.register)
-router.get('/getAdmin',Admin.getAdmin)
-router.post('/login',Admin.login)
-
-module.exports=router;
--- Api/Routes/DayCard/DayCardRoute.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const DayCard=require('../../Controller/DayCard/DayCardController')
-
-router.post('/register',DayCard.register);
-router.post('/getDayCard',DayCard.getCardImg);
-
-module.exports=router;
